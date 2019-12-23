5ff39067e8212d89c0e095ef0fcde267b3784fda
Luc de wit <luc.dewit@hotmail.nl>
2019-12-23 22:41:11 +0100

    ignored node_modules

--- start.js
+++ start.js
@@ -1,3 +1,4 @@
+//home play leaderboard news explore
 const express = require('express');
 const { resolve } = require("path") 
 
@@ -8,9 +9,20 @@ const app = new express();
 
 app.use(express.static(resolve(__dirname, "app")));
 
-app.get('/play.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+//stylesheet
+app.get('/style.css',(request,response)=>{response.sendFile('app/style.css',{root: __dirname})});
+
+//sub pages
 app.get('/home.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/play.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/leaderboard.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/news.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/explore.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+
+//default page
 app.get('/',(request,response)=>{response.sendFile('app/home.html',{root: __dirname});});
+
+
 
 app.on('listening', () => {
 	console.log(`SERVER RUNNING ON ${PORT}`)

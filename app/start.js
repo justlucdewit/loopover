574787876c2cb6dbaf1ddcbd4939b65365421053
Luc de wit <luc.dewit@hotmail.nl>
2019-12-23 20:00:12 +0100

    fixed it mayber

--- app/start.js
+++ app/start.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = new express();
 
 app.get('/', (request, response) => {
-	response.sendFile('index.html');
+	response.send("hello heroku");
+	//response.sendFile('index.html');
 });
+
+app.listen(process.env.PORT || 5000)

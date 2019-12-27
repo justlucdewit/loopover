cd6dc3933b9aa72c7c14c7b9ce6ae01750c709fe
Luc de wit <luc.dewit@hotmail.nl>
2019-12-27 16:32:51 +0100

    home content

--- start.js
+++ start.js
@@ -1,11 +1,8 @@
-//home play leaderboard news explore
 const express = require('express');
 const { resolve } = require("path") 
 
-
 const PORT = process.env.PORT || 5000;
 const app = new express();
-
 
 app.use(express.static(resolve(__dirname, "app")));
 
@@ -13,14 +10,36 @@ app.use(express.static(resolve(__dirname, "app")));
 app.get('/style.css',(request,response)=>{response.sendFile('app/style.css',{root: __dirname})});
 
 //sub pages
-app.get('/home.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
-app.get('/play.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
-app.get('/leaderboard.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
-app.get('/news.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
-app.get('/explore.html',(request, response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/home.html',(request, response)=>{
+	console.log("someone opened home.html");
+	response.sendFile('app/home.html',{root: __dirname});
+});
+
+app.get('/play.html',(request, response)=>{
+	console.log("someone opened play.html");
+	response.sendFile('app/home.html',{root: __dirname});	
+});
+
+app.get('/leaderboard.html',(request, response)=>{
+	console.log("someone opened leaderboard.html");
+	response.sendFile('app/home.html',{root: __dirname});
+});
+
+app.get('/news.html',(request, response)=>{
+	console.log("someone loaded news.html");
+	response.sendFile('app/home.html',{root: __dirname});
+});
+
+app.get('/explore.html',(request, response)=>{
+	console.log("someone loaded explore.html");
+	response.sendFile('app/home.html',{root: __dirname});
+});
 
 //default page
-app.get('/',(request,response)=>{response.sendFile('app/home.html',{root: __dirname});});
+app.get('/',(request,response)=>{
+	console.log("someone opened home.html");
+	response.sendFile('app/home.html',{root: __dirname});
+});
 
 
 

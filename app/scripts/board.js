777470abeb3b5438a9ad570cef436d1d729374dd
Luc de wit <luc.dewit@hotmail.nl>
2019-12-28 21:35:01 +0100

    added board rendering

--- app/scripts/board.js
+++ app/scripts/board.js
@@ -32,7 +32,16 @@ function Board(x, y){
     this.moveY = (colNum, count) => {
         const col = [...Array(this.height)].map((_, i) => this.cells[i][colNum]);
         for (let i = 0; i < this.height; i++){
-            
+            this.cells[i][colNum] = col[(((i+this.height*16-count)%this.height)+this.height)%this.height];
         }
+    }
+
+    this.isSolved = () => {
+        for (let [rowIndex, row] of this.cells.entries()){
+            for (let [col, tile] of row.entries()){
+                if (tile != rowIndex*row.length+col) return false;
+            }
+        }
+        return true;
     }
 }
--- app/scripts/loopover.js
+++ app/scripts/loopover.js
@@ -1,4 +1,34 @@
-let b1 = new Board(5, 5);
+const cols = 5;
+const rows = 5;
 
-b1.move(0, 0, 1);
-b1.show();
+const board = new Board(cols, rows);
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const width = 600;
+const height = 600;
+const tilesize = width/cols|0;
+
+//set canvas settings
+ctx.textBaseline = "middle";
+ctx.textAlign = "center";
+
+//set canvas size
+canvas.width = width;
+canvas.height = height;
+
+function renderBoard(){
+    ctx.clearRect(0, 0, width, height);
+    for (let x = 0; x < board.width; x++){
+        for (let y = 0; y < board.height; y++){
+            const index = board.cells[x][y];
+            const cx = (index%board.width+0.1)/(board.width-0.6);
+            const cy = (((index/board.width)|0)+0.2)/(board.height-0.6);
+            let color = [(1-cx)*235+15, cy*210+cx*(1-cy)*50+15, cx*220];
+            color = `rgb(${color.map(x=>x|0).join()})`;
+            ctx.fillStyle = color;
+            ctx.fillRect(x*tilesize | 0, y*tilesize | 0, tilesize, tilesize);
+        }
+    }
+}
+
+renderBoard();
